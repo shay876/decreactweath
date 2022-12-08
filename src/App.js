65b6c6d4b7f7cf99287a35596e064646774b4699
shay876 <111309646+shay876@users.noreply.github.com>
2022-12-08 14:26:48 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        <a href="/" target="_blank">
-          {" "}
-          Open Source by Shayla
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          <a href="/" target="_blank">
+            {" "}
+            Open Source by Shayla
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
