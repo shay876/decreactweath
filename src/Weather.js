b1b8915537210ff08783cd27bad8941df8080464
shay876 <111309646+shay876@users.noreply.github.com>
2022-12-08 18:15:52 -0700

    import axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,52 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+
+      date: "Thursday, Dec 8, 2022",
+      city: response.data.name,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Search..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Search..."
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>Calgary</h1>
-      <ul>
-        <li> Thursday, December 8, 2022</li>
-        <li>Mostly Cloudy</li>
-      </ul>
+          <div className="col-6">
+            <img src="#" alt="weathericon" />
 
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="weathericon"
-          />
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit">C</span>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
 
-          <span className="temperature">0</span>
-          <span className="unit">C</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 50%</li>
-            <li>Humidity: 60%</li>
-            <li>Wind: 5m/s</li>
-          </ul>
+              <li>Wind: {Math.round(weatherData.wind)}m/s</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "5fa5cad21a0752ec2fc6f4a9a07699e2";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${weatherData.city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return "loading...";
+  }
 }
